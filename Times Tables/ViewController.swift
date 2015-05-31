b17d384a052e3c78fcd3c3ecca03d268f8eed4c0
dadreddy20 <dadreddy20@gamil.com>
2015-05-31 07:42:52 +0530

    Initial Commit

---
+++ Times Tables/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Times Tables
+//
+//  Created by Anudeep Reddy Dwaram on 31/05/15.
+//  Copyright (c) 2015 iDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
