0372848e8b21ed657f6074579d2be7d5fb714f85
dadreddy20 <dadreddy20@gamil.com>
2015-05-31 08:59:21 +0530

    1 to 20 Tables

--- Times Tables/ViewController.swift
+++ Times Tables/ViewController.swift
@@ -8,8 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+	@IBOutlet weak var sliderValue: UISlider!
+	
+	@IBOutlet weak var table: UITableView!
+	
+	@IBAction func sliderMoved(sender: AnyObject) {
+		table.reloadData()
+	}
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 20;
+	}
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+		let timesTable = Int(sliderValue.value * 20)
+		// http://commandshift.co.uk/blog/2014/06/11/understanding-optionals-in-swift/
+		cell.textLabel?.text = "\(timesTable * (indexPath.row + 1))"
+		
+		return cell;
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
